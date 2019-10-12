98fe82c4f975dd0683b05c32439d1627fcbf9683
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-13 04:42:20 +0900

    ch03 learn about state, style, useState [ Say ]

--- ch03/first-component/src/Counter.js
+++ ch03/first-component/src/Counter.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 
 export default class Counter extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            number: 0
-        };
-    }
+    state = {
+        number: 0,
+        fixedNumber: 0,
+    };
 
     render() {
-        const {number} = this.state;
+        const { number, fixedNumber } = this.state;
         return (
             <div>
                 <h1>{number}</h1>
-                <button onClick={() => this.setState({number: number + 1})}> +1</button>
+                <button onClick={() => this.setState({number: number + 1}, () => {
+                    console.log('방금 setState가 호출되었습니다');
+                    console.log(this.state);
+                } )}> +1</button>
             </div>
         );
     }
