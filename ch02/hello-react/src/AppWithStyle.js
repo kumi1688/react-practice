407ff4e6e9aa26f9c9937d646bc22bae3d819029
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-12 20:32:41 +0900

    learn about styling [ AppWithStyle ]

---
+++ ch02/hello-react/src/AppWithStyle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App(){
+    const name = '리액트';
+    const style = {
+        backgroundColor:    'black',
+        color:  'aqua',
+        fontSize:   '48px',
+        fontWeight: 'bold',
+        padding:    16
+    };
+    return <div style={style}>{name}</div>;
+}
---
+++ ch02/hello-react/src/undefined.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+
+export default  function App(){
+    const name = undefined;
+    return <div>{name || '리액트입니다'}</div>
+}
