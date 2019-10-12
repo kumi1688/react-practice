572e2c4d29a888d8f87079f13648c2e9342a0540
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-12 20:23:59 +0900

    learn about three-address calculation [ app3 ]

--- ch02/hello-react/src/App.js
+++ ch02/hello-react/src/App.js
@@ -1,12 +1,13 @@
 import React, {Fragment} from 'react';
 
 export default function App() {
+    const name = '나는 최고다';
   return(
       <Fragment>
           <div>
               Hello <b>react</b>
           </div>
-          <h1>리액트 안녕!</h1>
+          <h1>{name}리액트 안녕!</h1>
           <h2>잘 작동하니?</h2>
       </Fragment>
   );
