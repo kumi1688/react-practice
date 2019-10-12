1fd285a07fb37fb1111751d4106b45ead952542b
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-13 04:05:04 +0900

    ch03 learn about component props, default props [ MyComponent ]

--- ch03/first-component/src/MyComponent.js
+++ ch03/first-component/src/MyComponent.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MyComponent = () => {
     return <div>나의 새롭고 멋진 컴포넌트</div>;
 };
 
 const MyComponentWithProps = (props) => {
-    return(
-        <div>안녕하세요, 제 이름은 {props.name} 입니다<br/>
-            children 값은 { props.children} 입니다
+    const {name, children, favoriteNumber } = props;
+    return (
+        <div>안녕하세요, 제 이름은 {name} 입니다<br/>
+            children 값은 {children} 입니다<br/>
+            제가 좋아하는 숫자는 {favoriteNumber} 입니다.
         </div>
     );
 };
 
 MyComponentWithProps.defaultProps = {
     name: '기본 이름',
+};
+
+MyComponentWithProps.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
 };
 
 export default MyComponentWithProps;
