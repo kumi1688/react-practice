c18dceac39b09894e91dfc8c78b7daa514a34693
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-13 18:20:19 +0900

    ch09 learning about Component Styling [ CSSModule ]

--- ch09/component_styling/src/App.js
+++ ch09/component_styling/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
 
 function App() {
   return (
       <div>
-        <SassComponent/>
+        <CSSModule/>
       </div>
   );
 }
