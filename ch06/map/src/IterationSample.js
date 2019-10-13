fccb95530706bf9455559341ba1b7e5ad9429fb5
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-13 15:08:25 +0900

    ch09 learning about Component Styling [ scss ]

--- ch06/map/src/IterationSample.js
+++ ch06/map/src/IterationSample.js
@@ -23,13 +23,21 @@ const IterationSample = () => {
         setNames(nextNames);
         setInputText('');
     };
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
 
     const nameList = names.map(name => <li key={name.id}>{name.text}</li>);
+    const namesList = names.map( name => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>{name.text}</li>
+    ));
+
     return (
         <>
             <input value={inputText} onChange={onChange} onKeyPress={onKeyPress}/>
             <button onClick={onClick} >추가</button>
-            <ul>{nameList}</ul>
+            <ul>{namesList}</ul>
         </>
     );
 };
