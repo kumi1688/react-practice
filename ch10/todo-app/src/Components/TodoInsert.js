5a83b01cc1697de239900dc42bca2279a6b748ed
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-14 05:47:52 +0900

    ch10 learning about insert Items [ App, ToDoInsert, TodoListItem, TodoList ]

--- ch10/todo-app/src/Components/TodoInsert.js
+++ ch10/todo-app/src/Components/TodoInsert.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ( {onInsert} ) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback( e => {
+        setValue(e.target.value);
+    }, []);
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
     return(
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
-            <button type="button">
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+            <button type="submit">
                 <MdAdd/>
             </button>
         </form>
