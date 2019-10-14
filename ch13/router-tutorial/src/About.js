ae2c3f712344b48b79b1e20cd847959d061678ad
kumi1688 <kumi1688@ajou.ac.kr>
2019-10-14 13:22:47 +0900

    ch13 learning about history [ App, HistorySample ]

--- ch13/router-tutorial/src/About.js
+++ ch13/router-tutorial/src/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({location}) => {
+    //문자열 맨 앞의 ? 생략
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
+    const showDetail = query.detail === 'true';
+
     return (
         <div>
             <h1>소개</h1>
             <p>이 프로젝트는 라우터 예제 프로젝트</p>
+            {showDetail && <p>디테일 값을 따로 설정함</p>}
         </div>
     );
 };
